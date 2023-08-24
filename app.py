0a251de9e0f16970ce7486698732811dc327be83
rukon-uddin <rukonuddin122@gmail.com>
2023-08-24 23:14:38 +0600

    Adding python code

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+stored_text = ""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send_text():
+    global stored_text
+    data = request.json
+    stored_text = data['text']
+    return jsonify({'message': 'Text received successfully'})
+
+@app.route('/receive')
+def receive_text():
+    global stored_text
+    return jsonify({'text': stored_text})
+
+if __name__ == '__main__':
+    app.run(debug=True, host="192.168.0.109")
