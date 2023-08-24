048b9b83f97f23f0870c23511721983d68d34b8c
rukon-uddin <rukonuddin122@gmail.com>
2023-08-24 23:39:58 +0600

    Added gunicorn

--- app.py
+++ app.py
@@ -19,6 +19,3 @@ def send_text():
 def receive_text():
     global stored_text
     return jsonify({'text': stored_text})
-
-if __name__ == '__main__':
-    app.run(debug=True, host="192.168.0.109")
