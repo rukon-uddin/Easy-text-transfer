a291d0928042f417f40dd5a4c85480eeae590d89
rukon-uddin <rukonuddin122@gmail.com>
2023-08-25 11:25:32 +0600

    Added uniqueID to get the message

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
 app = Flask(__name__)
 
-stored_text = ""
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+stored_text = {}
 
 @app.route('/')
 def index():
@@ -12,14 +17,29 @@ def index():
 def send_text():
     global stored_text
     data = request.json
-    stored_text = data['text']
+    stored_text[data['uid']] = {'text': data['text'], 'timestamp': time.time()}
+    print(stored_text)
     return jsonify({'message': 'Text received successfully'})
 
-@app.route('/receive')
+@app.route('/receive', methods=['POST'])
 def receive_text():
     global stored_text
-    return jsonify({'text': stored_text})
+    data = request.json
+    print(stored_text)
+    try:
+        text = stored_text[data["uid"]]["text"]
+    except:
+        text = "ZqgQ9QOE2$sq5kr8p3Vg*GgGNq&"
+    return jsonify({'text': text})
 
+def clear_stored_data():
+    global stored_text
+    current_time = time.time()
+    keys_to_delete = [key for key, value in stored_text.items() if (current_time - value['timestamp']) > 250]
+    for key in keys_to_delete:
+        del stored_text[key]
+
+scheduler.add_job(clear_stored_data, 'interval', minutes=5)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
